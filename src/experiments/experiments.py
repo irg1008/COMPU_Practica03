c851f45cecbbdd7efe9672f0057fd31f16843e6a
ChocoAlmendrado <pss1005@alu.ubu.es>
2021-11-15 22:38:18 +0100

    Experiments compinations

--- src/experiments/experiments.py
+++ src/experiments/experiments.py
@@ -3,4 +3,32 @@
 # Experiment with number of iterations.
 
 def experiments():
-  
+    TH = [2, 6, 8, 10]
+    MITH = [1, 2, 4, 5]
+    MUTSH = [1, 2, 4, 5]
+    CXPB = [0.5, 0.6, 0.8, 0.9]
+    MUTPB = [0.1, 0.15, 0.25, 0.3]
+
+    base = (5, 3, 3, 0.7, 0.2)
+    comb = (TH, MITH, MUTSH, CXPB, MUTPB)
+    names = (
+        "Tree height",
+        "Max Initial Tree Height",
+        "Max Mutation Subtree Height",
+        "Crois Probability",
+        "Mutation Probability"
+    )
+
+    for i in range(len(base)):
+        experimento = list(base)
+        experimento[i] = comb[i]
+
+        for j in experimento[i]:
+            aux = list(experimento)
+            aux[i] = j
+            
+            # Aqui ejecutamos la funcion con los experimentos de aux
+            
+            # Aqui guardamos las imágenes en ficheros
+            ngraph = "{names[i]} - {aux[i]} - graph"  # Nombre del grafico
+            ntree = "{names[i]} - {aux[i]} - tree"  # Nombre del arbol
