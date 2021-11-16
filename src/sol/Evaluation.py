7c092416cc14659fc3813953fb0876cbe01459fc
irg1008 <ivansudevlop@gmail.com>
2021-11-16 11:40:58 +0100

    Multiobjective

--- src/sol/Evaluation.py
+++ src/sol/Evaluation.py
@@ -1,65 +1,20 @@
 from sklearn import metrics
-import random
 
 
 def eval_ind_simple(inputs, targets, toolbox, individual):
     func = toolbox.compile(expr=individual)
     guessed_equals_target = 0
-    
-    min_limit, max_limit = min(targets), max(targets)
-    penalty = 0
+    guesses = []
 
     for input, target in zip(inputs, targets):
         guessed_target = round(func(*input))
+        guesses.append(guessed_target)
 
-        if guessed_target == target:
+        if guessed_target != target:
             guessed_equals_target += 1
-                        
-        if not min_limit <= guessed_target <= max_limit:
-            # Get distance outside interval between min and max.
-            distance = abs(guessed_target - min_limit) + abs(guessed_target - max_limit)
-            # Makes the convergence easier, but mutation takes toll.
-            # Thats why we use a random value.
-            if random.random() < 1:
-                penalty += distance
 
-    return guessed_equals_target/len(inputs), penalty
+    # The Hamming loss is the fraction of labels that are incorrectly predicted.
+    hamming_loss = metrics.hamming_loss(targets, guesses)
+    f1_score = metrics.f1_score(targets, guesses, average="micro")
 
-# def eval_ind_advanced(inputs, targets, toolbox, individual):
-#     # Sensitivity and specificity.
-#     a, b = 1, 1
-#     n = len(inputs)
-
-#     func = toolbox.compile(expr=individual)
-
-#     # Correct and wrong classifications.
-#     wrong_guesses = 0
-
-#     for input, target in zip(inputs, targets):
-#         guess = func(*input)
-#         type = target[0]
-
-#         if int(guess) != type:
-#             wrong_guesses += 1
-
-#     acc = (n - wrong_guesses) / n
-
-#     return acc,
-
-# def eval_ind_confussion(inputs, targets, toolbox, individual):
-#     func = toolbox.compile(expr=individual)
-
-#     test = []
-#     guesses = []
-
-#     # Make the confusion matrix.
-#     for input, target in zip(inputs, targets):
-#         guess = func(*input)
-#         type = target[0]
-
-#         guesses.append(int(guess))
-#         test.append(type)
-
-#     f1_score = metrics.f1_score(test, guesses, average="weighted")
-
-#     return f1_score,
+    return f1_score, guessed_equals_target
